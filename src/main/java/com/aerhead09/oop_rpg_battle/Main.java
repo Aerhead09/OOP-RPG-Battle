d905c8cc45dc6746a2864a3301542a9812ddd1cc
Aerhead09 <Jeankisekiwaluyo09@gmail.com>
2026-06-27 23:22:23 +0700

    ubah ke struktur maven

---
+++ src/main/java/com/aerhead09/oop_rpg_battle/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop_rpg_battles;
+
+/**
+ *
+ * @author jeany
+ */
+public class OOP_RPG_Battles {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
